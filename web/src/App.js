01da46cce167f327c222fd064dc9b52b8e3600c5
Michael Martinez <soporte@mi-martinez.com>
2023-02-17 19:25:48 -0500

    Add component search :+1:

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,8 @@
-import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
-import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
-
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider, createTheme } from '@mui/material/styles'
+
+import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
+import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
 
 import FatalErrorPage from 'src/pages/FatalErrorPage'
 import Routes from 'src/Routes'
@@ -20,19 +20,22 @@ const themeDH = createTheme({
     error: {
       main: '#ff1744',
     },
+    black: {
+      main: '#000000',
+    },
   },
 })
 
 const App = () => (
   <ThemeProvider theme={themeDH}>
-  <CssBaseline />
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <Routes />
-      </RedwoodApolloProvider>
-    </RedwoodProvider>
-  </FatalErrorBoundary>
+    <CssBaseline />
+    <FatalErrorBoundary page={FatalErrorPage}>
+      <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+        <RedwoodApolloProvider>
+          <Routes />
+        </RedwoodApolloProvider>
+      </RedwoodProvider>
+    </FatalErrorBoundary>
   </ThemeProvider>
 )
 
