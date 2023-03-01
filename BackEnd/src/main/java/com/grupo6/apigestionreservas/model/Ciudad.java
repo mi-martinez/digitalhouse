1adc0b013ce2ceefa33157d5f485215b135458a0
JuanManuelBD <jumeight@gmail.com>
2023-03-01 07:29:04 -0300

    Mapped Producto, Ciudad, Caracteristica and Imagen to database and added relationship between Categoria and Producto

---
+++ BackEnd/src/main/java/com/grupo6/apigestionreservas/model/Ciudad.java
@@ -0,0 +1,34 @@
+package com.grupo6.apigestionreservas.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+public class Ciudad {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombre;
+
+    @OneToMany(mappedBy = "ciudad")
+    private Set<Producto> productos;
+
+}
+
---
+++ BackEnd/src/main/java/com/grupo6/apigestionreservas/model/Producto.java
@@ -0,0 +1,59 @@
+package com.grupo6.apigestionreservas.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+public class Producto {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String titulo;
+
+    @Column
+    private Double latitud;
+
+    @Column
+    private Double longitud;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ciudad_id", nullable = false)
+    private Ciudad ciudad;
+
+    @ManyToMany
+    @JoinTable(
+            name = "caracteristica_producto",
+            joinColumns = @JoinColumn(name = "producto_id"),
+            inverseJoinColumns = @JoinColumn(name = "caracteristica_id")
+    )
+    private Set<Caracteristica> caracteristicas;
+
+    @OneToMany(mappedBy = "producto")
+    private Set<Imagen> imagenes;
+
+
+}
